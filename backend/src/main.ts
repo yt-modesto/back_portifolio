55d81747b7ce5119f0c420971d8b9b3385cf06fc
yt-modesto <clodoaldo.neto.116@ufrn.edu.br>
2023-03-10 16:50:13 -0300

    enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: [
+        'http://localhost:3001',
+        /^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):3001$/,
+      ],
+    },
+  });
+  app.enableCors({
+    origin: [
+      'http://localhost:3001',
+      /^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):3001$/,
+    ],
+  });
   await app.listen(3000);
 }
 bootstrap();
